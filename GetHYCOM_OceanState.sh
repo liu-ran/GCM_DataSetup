5f00b65a7ee3a08cd425d699f78252bf928167b0
Joe <Joe>
2018-01-30 07:31:38 -0700

    Cleaned up GetHYCOM_OceanState.sh

--- GetHYCOM_OceanState.sh
+++ GetHYCOM_OceanState.sh
@@ -8,12 +8,35 @@ x2=3160
 y1=1580
 y2=2300
 
+
+declare -a vars=("2d" "salt" "temp" "uvel" "vvel")
+
 for experiment in "${exps[@]}"
 do
 
   echo $experiment
-  wget ftp://ftp.hycom.org/datasets/GLBa0.08/${experiment}/data/2d/*.nc
 
-  ncks -d lon,0 -d lon,399 -d lat,0 -d lat,399 in.nc out.nc
+  for var in "${vars[@]}"
+  do
+
+    if [ ! -d "$var/" ]; then
+      mkdir $var
+    fi
   
+    echo "wget ftp://ftp.hycom.org/datasets/GLBa0.08/${experiment}/data/${var}/*.nc"
+    wget ftp://ftp.hycom.org/datasets/GLBa0.08/${experiment}/data/${var}/*.nc
+  
+    # Trim down the file to extract the region
+    for file in $(ls *.nc)
+    do
+  
+      echo "ncks -d X,$x1,$x2 -d Y,$y1,$y2 $file ${var}/regional_${file}"
+      ncks -d X,$x1,$x2 -d Y,$y1,y2 $file "${var}/regional_${file}"
+      rm $file
+  
+    done
+
+  done
+
 done
+
